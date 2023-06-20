a3e07b698fdcfa25c69919fdcf83651b5d5df126
maphadi <maphadinr@gmail.com>
2023-06-20 08:29:19 -0700

    02

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
-
-
+/**
+ * main - se if n is negative, positive or just zero
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
