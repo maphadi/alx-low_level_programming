5e0ec00096a70cec6d07bf5c3f8d92660a22ae40
maphadi <maphadinr@gmail.com>
2023-06-20 11:53:57 -0700

    06

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Determines if a number is positive, negative or zero.
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main (void)
+{
+	int n;
+	n=0;
+	while (n<10)
+	{
+	putchar("%d", n);
+	n++;
+
+	return(0);
+}
+
