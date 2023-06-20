7670ad14afbaf00f391bc8cc94a542e61e0e9cdd
maphadi <maphadinr@gmail.com>
2023-06-20 08:43:46 -0700

    02

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
         {
         printf("Last digit of %d is and is 0\n", n);
         }
-	if ( (n &10) < 6 ||last_digit != 0 )
+	if ( (n &10) < 6 || (n &10) != 0 )
         {
         printf("Last digit of %d is and is less than 6 and not 0\n", n);
         }
