345bebcda004f548ec9957361d31064ac55372a4
maphadi <maphadinr@gmail.com>
2023-06-21 08:21:33 -0700

    okk

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * maain - NOT e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char s3[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+	for (i=0 ; i<24; i++)
+	{
+	putchar(s3[24]);
+	}
+	putchar('\n');
+	return(0);
+
+}
+
