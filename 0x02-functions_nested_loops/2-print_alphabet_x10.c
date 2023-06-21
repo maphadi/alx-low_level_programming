592f0f2b530cd2cd1968aa2d3a17b73bdb4eca19
maphadi <maphadinr@gmail.com>
2023-06-21 15:44:13 -0700

    please

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void);
+{
+	char alpha;
+	int  i =1;
+	for ( i = 1; i <= 10; i++)
+	{
+		for  ( alpha = 'a' ; alpha <= 'z' ; alpha++)
+		{
+		_putchar(alpa)	
+ 		}
+	}
+
+
+
+
+
+}
+
