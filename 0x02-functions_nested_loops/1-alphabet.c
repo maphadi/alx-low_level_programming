c1a5f2b07148c43bee53117bc5eb369bd5266409
maphadi <maphadinr@gmail.com>
2023-07-10 04:52:31 -0700

    fixing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,14 @@
 #include "main.h"
 
 /**
- * main - alphabet
+ * main - print_alphabet
 */
+
 void print_alphabet(void)
 {
 	char alp;
-	for ( alp = 'a' ; alp <= 'z' ; alp++)
+
+	for (alp = 'a' ; alp <= 'z' ; alp++)
 	_putchar(alp);
 	_putchar('\n');
 }
