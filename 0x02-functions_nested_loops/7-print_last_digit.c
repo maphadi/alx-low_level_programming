85695d22a26d080794c2b3bfacf0885b125f2885
maphadi <maphadinr@gmail.com>
2023-07-11 08:16:16 -0700

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,17 +9,17 @@
 int print_last_digit(int i)
 {
 	int j;
-	
+
 	j = i % 10;
-	if ( j < 0 )
+	if (j < 0)
 	{
-	_putchar(-j + 48 );
-	return(-j);
+	_putchar (-j);
+	return (-j);
 	}
 	else
 	{
-	_putchar ( j + 48 );
-	return(j);
+	_putchar (j);
+	return (j);
 	}
 }
 
