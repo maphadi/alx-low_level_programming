392ee05a22ff6fbfa6dd43feaf730cb9ef2a3c1b
maphadi <maphadinr@gmail.com>
2023-07-11 07:07:37 -0700

    fix

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,17 +1,14 @@
 #include "main.h"
 
-
 /**
  *upper andlower
  * Return: 1 if char is lowercase, otherwise 0.
 */
-
-
-	int _isalpha(int c)
+int _isalpha(int c)
 {
-		if (c >= 65 && c <= 122)
-			return (1);
-		else
-			return (0);
+	if (c >= 65 && c <= 122)
+	return (1);
+	else
+	return (0);
 }
 
