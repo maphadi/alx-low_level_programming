71adc094d24d9caf4fd247e46712229992cadf40
maphadi <maphadinr@gmail.com>
2023-07-13 15:13:58 -0700

    fixing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,17 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	j = 0;
-	while (j < 10)
+	for (i = 0; i <= 9; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar (i);
+			if (j >= 10)
+			{
+				_putchar ('1');
+			}
+				_putchar  (j % 10 + '0');
 		}
-		_putchar('\n');
-		j++;
+	_putchar ('\n');
 	}
 }
 
