48e18131f0fd4aaed38dc4e7f791b532ad26d408
maphadi <maphadinr@gmail.com>
2023-07-15 09:18:28 -0700

    fixing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - make a diagonal line
+ *
+ * @n: number of rows and colums
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+	_putchar ('\n');
+	}
+	else
+	{
+		for  (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+			if (j == i)
+			_putchar ('\\');
+			else if (j < i)
+			_putchar (' ');
+			}
+		_putchar ('\n');
+		}
+	}
+}
+
