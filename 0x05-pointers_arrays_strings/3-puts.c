7fae746406cadd7e6d0649277f5795f1909a27d0
maphadi <maphadinr@gmail.com>
2023-07-18 11:38:09 -0700

    0x05

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
-/*
- *Write a function that prints a string, followed by a new line, to stdout.
- *Prototype: void _puts(char *str);
+/**
+ * _puts - print a string
+ * @str: string
  */
 
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-	_putchar (*str++) ;
+	_putchar (*str);
+	str++;
 	}
 	_putchar ('\n');
 }
